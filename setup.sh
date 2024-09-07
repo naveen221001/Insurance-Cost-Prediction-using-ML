c302a3e23e65ad3936c40aee35928220dd2d6302
NAVEEN KUMAR CHAMARIA <72919423+naveen221001@users.noreply.github.com>
2024-09-08 02:22:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"naveenchamaria2001@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
