9f27bac73275580db4291632a490158578865cd2
NAVEEN KUMAR CHAMARIA <72919423+naveen221001@users.noreply.github.com>
2024-09-08 02:48:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep  8 00:41:37 2024
-
-@author: DELL
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Sun Sep  8 00:00:10 2024
 
 @author: DELL
 """
-import pickle
+
 import streamlit as st
-import joblib
-pickle_in = open(‘classifier.pkl’, ‘rb’)
-classifier = pickle.load(pickle_in)
+import pickle
 
-@st.cache()
-
+@st.cache
+def load_model():
+    with open('classifier.pkl', 'rb') as pickle_in:
+        model = pickle.load(pickle_in)
+    return model
 
 def main():
     html_temp = """
@@ -31,26 +25,20 @@ def main():
     
     st.markdown(html_temp, unsafe_allow_html=True)
     
-    model = joblib.load('model_joblib_gr')
-
- 
+    # Load the model
+    model = load_model(model_joblib_gr)
+    
     # Input fields
     p1 = st.slider("Enter Your Age", 18, 100)
  
     s1 = st.selectbox("Sex", ("Male", "Female"))
-    if s1 == "Male":
-        p2 = 1
-    else:
-        p2 = 0
+    p2 = 1 if s1 == "Male" else 0
 
     p3 = st.number_input("Enter Your BMI Value")
     p4 = st.slider("Enter Number of Children", 0, 4) 
  
     s2 = st.selectbox("Smoker", ("Yes", "No"))
-    if s2 == "Yes":
-        p5 = 1
-    else:
-        p5 = 0
+    p5 = 1 if s2 == "Yes" else 0
      
     p6 = st.slider("Enter Your Region [1-4]", 1, 4)
  
@@ -58,7 +46,7 @@ def main():
     if st.button('Predict'):
         prediction = model.predict([[p1, p2, p3, p4, p5, p6]])
         st.balloons()
-        st.success('Insurance Amount is {} '.format(round(prediction[0], 2))) 
+        st.success('Insurance Amount is ${:.2f}'.format(prediction[0])) 
     
 if __name__ == '__main__':
     main()
